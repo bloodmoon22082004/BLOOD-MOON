1c0992c72645084f4de0cce6467023591d81a4fc
BLOOD MOON <114036681+bloodmoon22082004@users.noreply.github.com>
2022-09-27 13:51:55 +1000

    Add files via upload

---
+++ 11.4D_name_tester.c
@@ -0,0 +1,36 @@
+// By BLOODMOON
+
+#include <stdio.h>
+#include <string.h>
+#include "terminal_user_input.h"
+
+#define LOOP_COUNT 60
+
+void print_silly_name(my_string name)
+{
+  printf("Your name %s is a ", name.str);
+  int index;
+  for(index=0;index<LOOP_COUNT;index++) {
+    printf("silly ");
+  }
+  printf("name!\n");
+}
+
+int main()
+{
+  my_string name;
+  int index;
+ 
+  name = read_string("What is your name? ");
+
+  if (strcmp(name.str, "Trung") == 0 || strcmp(name.str, "Matthew") == 0)
+  {
+    printf("Your name is an AWESOME name!");  
+  }
+  else
+  {
+    print_silly_name(name);
+  }
+
+  return 0;
+}
